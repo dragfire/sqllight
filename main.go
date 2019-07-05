10c5a5d71f22f7d30b353012d68cd9fc7c6b6faf
Devajit Asem <asem.devajit@gmail.com>
2019-07-05 16:41:48 +0000

    Add perform meta commands

--- main.go
+++ main.go
@@ -1,36 +1,69 @@
 package main
 
 import (
-	"bufio"
-	"fmt"
-	"log"
-	"os"
+    "bufio"
+    "fmt"
+    "log"
+    "os"
+    "strings"
 )
 
 const (
-	cmdExit = ".exit"
-	cmdHelp = ".help"
+    cmdExit = ".exit"
+    cmdHelp = ".help"
 )
 
-func displayLeftDbName() {
-	fmt.Print("sqllight > ")
+type MetaCommandResult int
+const (
+    MetaCommandSuccess MetaCommandResult = iota
+    MetaCommandUnrecognizedCommand
+)
+
+type PrepareResult int
+const (
+    PrepareSuccess PrepareResult = iota
+    PrepareUnrecognizedStatement
+)
+
+type StatementType int
+const (
+    StatementTypeInsert StatementType = iota
+    StatementTypeSelect
+)
+
+type Statement struct {
+    statementType StatementType
+}
+
+func displayPrompt() {
+    fmt.Print("sqllight > ")
+}
+
+func performMetaCommand(cmd string) MetaCommandResult {
+    if strings.Compare(cmd, ".exit") == 0 {
+	os.Exit(0)
+    }
+    return MetaCommandUnrecognizedCommand
 }
 
 func main() {
-	scanner := bufio.NewScanner(os.Stdin)
-	displayLeftDbName()
-	for scanner.Scan() {
-		cmd := scanner.Text()
-		switch cmd {
-		case cmdExit:
-			os.Exit(0)
-		default:
-			fmt.Println("Unknown command: ", cmd)
-		}
-		displayLeftDbName()
+    scanner := bufio.NewScanner(os.Stdin)
+    displayPrompt()
+    for scanner.Scan() {
+	cmd := scanner.Text()
+	if strings.LastIndex(cmd, ".") == 0 {
+	    switch performMetaCommand(cmd) {
+	    case MetaCommandSuccess:
+		break
+	    case MetaCommandUnrecognizedCommand:
+		fmt.Println("Unknown command: ", cmd)
+		break
+	    }
 	}
+	displayPrompt()
+    }
 
-	if scanner.Err() != nil {
-		log.Panic(scanner.Err())
-	}
+    if scanner.Err() != nil {
+	log.Panic(scanner.Err())
+    }
 }
